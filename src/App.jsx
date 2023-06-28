0c93e11f1ae00da4ee89bb4dfcc0cb82b73459d6
ItsFaizan <faizanjavid71@gmail.com>
2023-06-28 07:54:13 +0500

    Validations added

--- src/App.jsx
+++ src/App.jsx
@@ -12,40 +12,53 @@ const App = () => {
 
   const inputs = [
     {
-      id:1,
-      name:"username",
-      type:"text",
-      placeholder:"Username",
-      label:"username"
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special character!",
+      label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
     },
     {
-      id:2,
-      name:"email",
-      type:"text",
-      placeholder:"Email",
-      label:"Email"
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "It should be a valid email address!",
+      label: "Email",
+      required: true,
     },
     {
-      id:3,
-      name:"birthday",
-      type:"text",
-      placeholder:"Birthday",
-      label:"Birthday"
+      id: 3,
+      name: "birthday",
+      type: "date",
+      placeholder: "Birthday",
+      label: "Birthday",
     },
     {
-      id:4,
-      name:"password",
-      type:"password",
-      placeholder:"Password",
-      label:"Password"
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage:
+        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+      label: "Password",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
     },
     {
-      id:5,
-      name:"confirmPassword",
-      type:"password",
-      placeholder:"Confirm Password",
-      label:"Confirm Password"
-    }
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      errorMessage: "Passwords don't match!",
+      label: "Confirm Password",
+      pattern: values.password,
+      required: true,
+    },
   ]
 
   const handleSubmit= (e) => {
